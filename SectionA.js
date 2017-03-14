adca6e698c4999557b629d4073b0c92bcc3e9fb6
Aiswaria <aiswaria.sivasankari@gmail.com>
2017-03-15 01:23:17 +1100

    adding files

---
+++ SectionA.js
@@ -0,0 +1,154 @@
+module.exports = {
+  'Verify Page Load' : function (browser, done) {
+	try{
+		browser
+		.windowMaximize()
+		.url('http://abc.net.au/news')
+		.waitForElementVisible('body', 1000);  
+	browser.execute("return document.readyState;", function(result) {
+  	  if(result.value = 'complete') {
+		console.log("Page Loaded Succesfully");
+	}
+	});   
+	} catch (myError) {
+		console.log(myError);
+	};
+  },	
+
+  'Verify News Banner' : function (browser, done) {
+	try{
+		browser
+			.useXpath()
+			//Locating the element "News Banner"
+			.waitForElementVisible('//*[@id="header"]/div[1]/a/img[2]', 1000);
+			console.log("News Banner is Present");
+	}catch(myError){
+		console.log(myError);
+	};
+ },
+
+  'Navigate to Just In' : function (browser, done) {
+	try{
+		browser
+			.useXpath()
+			//Locating the menu item "JustIn"
+			.waitForElementVisible('//*[@id="n-justin"]/a', 5000)
+			.click('//*[@id="n-justin"]/a')
+			//Waiting for the page header with text "JustIn" to bet displayed
+			.waitForElementVisible('//*[@id="container_subheader"]/div/div/h1', 5000)
+			.verify.containsText('//*[@id="container_subheader"]/div/div/h1', 'Just In');
+			
+	} catch (myError){
+		console.log(myError);
+	};
+ },
+
+  'Verify Author Present for every Article' : function (browser, done) {
+	try{
+		function iter(elements){
+			if(elements.value.length>1){
+				console.log('function called');	
+				console.log(elements.value.length);	
+				//Finding all the article titles and comparing it with the number of author elements retrieved
+				browser.elements('xpath', '//*[@id="main_content"]/div[2]/div/div[1]/div/ul/li/h3', function(newElements){
+					console.log(newElements.value.length);
+					browser.verify.equal(elements.value.length, newElements.value.length);
+        			})
+		}
+		};
+		browser
+			.useXpath()
+			.waitForElementVisible('//*[@id="main_content"]/div/div/div[1]/div/ul/li', 5000)
+			.elements('xpath','//*[@id="main_content"]/div[2]/div/div[1]/div/ul/li/div', iter);
+		}catch (myError){
+			console.log(myError);
+		};
+ },
+ 
+  'Verify TimeStamp Present for every Article' : function (browser, done) {
+	try{
+		function iter(elements){
+			if(elements.value.length>1){
+				console.log('function called');	
+				console.log(elements.value.length);	
+				//Finding all the article titles and comparing it with the number of Timestamp elements retrieved
+				browser.elements('xpath', '//*[@id="main_content"]/div[2]/div/div[1]/div/ul/li/h3', function(newElements){
+					console.log(newElements.value.length);
+					browser.verify.equal(elements.value.length, newElements.value.length);
+        			})
+		}
+		};
+		browser
+			.useXpath()
+			.waitForElementVisible('//*[@id="main_content"]/div/div/div[1]/div/ul/li', 5000)
+			.elements('xpath','//*[@id="main_content"]/div[2]/div/div[1]/div/ul/li/p[1]', iter);
+		}catch (myError){
+			done(myError);
+		};
+ },
+
+ 'Verify Text Present for every Article' : function (browser, done) {
+	try{
+		function iter(elements){
+			if(elements.value.length>1){
+				console.log('function called');	
+				console.log(elements.value.length);	
+				//Finding all the article titles and comparing it with the number of Text elements retrieved
+				browser.elements('xpath', '//*[@id="main_content"]/div[2]/div/div[1]/div/ul/li/h3', function(newElements){
+					console.log(newElements.value.length);
+					browser.verify.equal(elements.value.length, newElements.value.length);
+        			})
+		}
+		};
+		browser
+			useXpath()
+			.waitForElementVisible('//*[@id="main_content"]/div/div/div[1]/div/ul/li', 5000)
+			.elements('xpath','//*[@id="main_content"]/div[2]/div/div[1]/div/ul/li/p[2]', iter);
+		}catch (myError){
+			console.log(myError);
+		};
+ },
+
+
+
+ 'Verify Video Load' : function (browser, done){
+	try{
+		//Finding the readyState of the video
+		browser
+			.url('http://www.abc.net.au/news/2017-02-09/weatherill-promises-to-intervene-dramatically/8254908')
+			.execute("document.getElementById('jwplayer-video-0').readyState;", function (result) {
+				if(result.value>0){
+					console.log("Video Loaded Succesfully");
+				}else{
+					console.log("Video Not Loaded");
+				}
+				});
+	}catch(myError){
+		done(myError);	
+	};
+ },
+
+ 'Verify the images displayed' : function (browser, done){
+	try{
+		function compare(result){
+			if(result.value.length>0){
+			result.value.forEach(function(element){
+				//Clicking the Right arrow for each image and waiting for the new image to load
+				browser
+				.click('//*[@id="main_content"]/div[2]/div/div/div/div/div/a[2]')
+				.waitForElementVisible('//*[@id="main_content"]/div[2]/div/div/div/div/ul', 5000);	
+		})
+		}
+	};
+		
+		browser
+			.url('http://www.abc.net.au/news/2017-02-10/abc-open-pic-of-the-week/8256256')
+			//Finding the total number of images present and waiting for them to load
+			.waitForElementVisible('//*[@id="main_content"]/div[2]/div/div/div/ul/li', 5000)
+			.elements('xpath', '//*[@id="main_content"]/div[2]/div/div/div/ul/li', compare)
+			.end();
+	}catch(myError){
+		done(myError);	
+	};
+ }
+};
---
+++ SectionB.js
@@ -0,0 +1,134 @@
+module.exports = {
+	
+	before: function(browser){
+		try{
+			browser
+				.windowMaximize()
+				.url('http://www.abc.net.au/radionational/')
+		}catch(myError){
+			done(myError);
+		};
+	},
+  'Verify Page Load' : function (browser) {
+	try{
+		browser
+			.waitForElementVisible('body', 1000);  
+		browser.execute("return document.readyState;", function(result) {
+			if(result.value = 'complete') {
+			console.log("Page Loaded Succesfully");
+		}
+		});   
+	} catch (myError) {
+		done(myError);;
+	};
+  },
+
+ 'Navigate to program' : function (browser){
+	try{
+		browser
+			.useXpath();
+		//Locationg and clicking the menu item "Programs"
+			console.log("Finding element: Programs");
+		browser
+			.waitForElementVisible('//*[@id="rn-navigation"]/ul/li[2]/a', 5000)
+			.click('//*[@id="rn-navigation"]/ul/li[2]/a');
+		//Locating the sub menu "A Big Country"
+			console.log("Finding sub menu: A Big Country");
+		browser
+			.waitForElementVisible('//*[@id="rn-programindex"]/li[1]/a', 5000);
+			
+	} catch (myError) {
+		done(myError);;
+	};
+
+ },
+
+ 'Navigate to Submenu: Big Ideas' : function (browser){
+	try{
+		//Locating and clicking sub menu "A Big Country"
+		console.log("Finding Sub menu: A Big Country");
+		browser
+			.waitForElementVisible('//*[@id="rn-programindex"]/li[3]/a', 5000)
+			.click('//*[@id="rn-programindex"]/li[3]/a');
+		//Asserting the header of the page
+		browser
+			.assert.containsText('//*[@id="header"]/div/div[1]/h2/a', 'A Big Country')
+			
+	} catch (myError) {
+		done(myError);;
+	};
+
+ },
+
+ 'Navigate to last item in Program Guide' : function (browser){
+	try{
+		// Navigating to the last element by trying to locate the button "View full Program guide" and then clicking on the last element 
+		
+		browser
+			.useXpath();
+		//Clicking on the "Home" icon
+		browser
+			.click('//*[@id="rn-navigation"]/ul/li[1]/a')		
+			.elements('xpath', '//*[@id="content"]/div[1]/div/div/div[2]/ul/li[8]', function(result){
+				if(result.value.length > 0){
+					for(var count =1; count < result.value.length; count++){
+						browser					   	
+						.isVisible('xpath' , '//*[@id="content"]/div[1]/div/div/div[2]/ul/li[22]/a/div', function(result){
+						if(result.value == -1){
+							browser.click('//*[@id="right-arrow"]');
+						}
+					})
+					.click('//*[@id="content"]/div[1]/div/div/div[2]/ul/li[22]/a/div');
+					}
+				}
+			});	
+	
+	} catch (myError) {
+		done(myError);;
+	};
+
+ },
+
+ 'Search For A Program' : function (browser){
+	try{
+		var text = 'infant stress';
+		browser
+			.useXpath();
+		//Clicking on the "Home" icon and then entering text in the search field and clicking Enter
+		browser
+			.click('//*[@id="rn-navigation"]/ul/li[1]/a')
+			.sendKeys('tag name', 'input', text)
+			.Keys.ENTER;
+		//Waiting for the article title to get displayed	
+		browser
+			.waitForElementVisible('//*[@id="content"]/div[1]/div/div[2]/ul/li[1]/div/div[1]/h3/a', 5000)
+			.assert.containsText('//*[@id="content"]/div[1]/div/div[2]/ul/li[1]/div/div[1]/h3/a', text);
+	} catch (myError) {
+		done(myError);;
+	};
+ },
+ 
+ 'Click on Share button and Verify the PopUp' : function (browser){
+	 try{
+		browser
+			.url('http://www.abc.net.au/radionational/programs/bigideas/a-fortunate-universe/8076406');
+			//Locating Share Button
+			console.log("Finding element: Share button");
+			browser
+			.waitForElementVisible('//*[@id="u_0_2"]/div/span[2]', 5000)
+			.click('xpathofSharebutton')
+			.timeoutsImplicitlyWait(5000, function(result){
+				browser.windowHandles(function(winHandle){
+					browser
+						.assert.equal(winHandle.value.length, 2)
+						.switchWindow(winHandle.value[1])
+						.assert.title('Facebook');
+				})
+			})
+	 }catch(myError){
+		 done(myError);;
+	 };
+ }
+};	
+
+  
